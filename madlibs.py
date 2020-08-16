62b31bd195620ce9421a2cc4c2cef1c8801d4741
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-08-16 15:15:10 +0300

    fix eroor

--- madlibs.py
+++ madlibs.py
@@ -1,11 +1,12 @@
 def main():
       time = input("Enter a number from 1 to 12: ")
+      items = input("Enter a noun (plural):")
       name = input("Enter a name: ")
       scream = input("Enter any sentence:  ")
-      action = input("Enter a verb:")
+      verb = input("Enter a verb:")
       print ("It was" + time + " o'clock when I heard a knock at the door.I opened the door and there was a box full of "
-      + items + "dolls with a note saying \"From Mr. " + name.capitalize()+".\"
-      Just as I closed the door I heard a scream "\" + scream.upper() + "\" I froze in place and all I could do " + verb +".")
+      + items + " with a note saying \"From Mr. " + name.title()+"\
+      Just as I closed the door I heard a scream \"" + scream.upper() + "\" I froze in place and all I could do " + verb +".")
 
 if __name__ == '__main__':
 	main()
