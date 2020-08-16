e5bc79a6f4248c6d4d933cffa27147a7e700cb65
Raghad Alrasheed <raghad@MacBook-Pro-alkhas-b-Raghad.local>
2020-08-16 14:04:52 +0300

    first code added

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,12 @@
 def main():
-	# write your code here
-
-
+time = input("Enter a number from 1 to 12: ") 	# write your code here
+items = input("Enter a noun (plural): ") 	# write your code here
+name = input("Enter a name: ") 	# write your code here
+scream = input("Enter any sentence:  ")
+action = input("Enter a verb:")
+print ("It was" + time + " o'clock when I heard a knock at the door.I opened the door and there was a box full of "
++ items + "dolls with a note saying \"From Mr. " + name.capitalize()+".\"
+Just as I closed the door I heard a scream "\" + scream.upper() + "\" I froze in place and all I could do " + verb +".")
 
 if __name__ == '__main__':
 	main()
